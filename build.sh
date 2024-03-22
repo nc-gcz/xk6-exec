7ead5f312e56b73008a060c3ab49658f33ac49ca
Grzegorz Czarnocki <gcz@netcompany.com>
2024-03-22 10:30:30 +0100

    Build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 
 go install go.k6.io/xk6/cmd/xk6@latest
 
+rm -rf ~/go/pkg/mod/github.com/nc-gcz
+
+ls -alh ~/go/pkg/mod/github.com/nc-gcz || true
+
+sleep 3
+
 xk6 build --with github.com/nc-gcz/xk6-exec@main
 
 ./k6 version
